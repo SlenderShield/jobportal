e200d0ab191128b8922ed1f1ad25980e632c8683
Muralidhara Bhat KS <ksmuralidhara0@gmail.com>
2025-03-20 22:35:15 +0530

    Add job seeker apply and job save Retrieve job save details

--- src/main/java/com/mdb/jobportal/entity/JobSeekerProfile.java
+++ src/main/java/com/mdb/jobportal/entity/JobSeekerProfile.java
@@ -149,6 +149,7 @@ public class JobSeekerProfile {
         this.skills = skills;
     }
 
+    @Transient
     public String getPhotosImagePath(){
         if(profilePhoto == null ||  userAccountId ==null){
             return null;
---
+++ src/main/java/com/mdb/jobportal/services/JobSeekerApplyService.java
@@ -0,0 +1,29 @@
+package com.mdb.jobportal.services;
+
+import com.mdb.jobportal.entity.JobPostActivity;
+import com.mdb.jobportal.entity.JobSeekerApply;
+import com.mdb.jobportal.entity.JobSeekerProfile;
+import com.mdb.jobportal.repository.JobSeekerApplyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobSeekerApplyService {
+
+    private JobSeekerApplyRepository jobSeekerApplyRepository;
+
+    @Autowired
+    public JobSeekerApplyService(JobSeekerApplyRepository jobSeekerApplyRepository) {
+        this.jobSeekerApplyRepository = jobSeekerApplyRepository;
+    }
+
+    public List<JobSeekerApply> getCandidatesJobs(JobSeekerProfile userAccountId) {
+        return jobSeekerApplyRepository.findByUserId(userAccountId);
+    }
+
+    public List<JobSeekerApply> getJobCandidates(JobPostActivity job) {
+        return jobSeekerApplyRepository.findByJob(job);
+    }
+}
